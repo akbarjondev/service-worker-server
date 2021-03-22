b05d2ea7bdf4a1ced8ad9a3a6184ec119c21c72a
akbarjondev <tojiyev.akbarjon@gmail.com>
2021-03-22 17:01:11 +0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,75 @@
+const cors = require('cors')
+const express = require('express')
+const pushManager = require('web-push')
+
+const port = process.env.PORT || 3001
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+const subscribers = []
+
+const { publicKey, privateKey } = pushManager.generateVAPIDKeys()
+
+pushManager.setVapidDetails(
+	"mailto:gimmick.data@gmail.com",
+	publicKey,
+	privateKey
+)
+
+app.get('/publickey', (req, res) => {
+
+	try {
+		
+		res.send({
+			publicKey
+		}).end()
+
+	} catch(e) {
+		console.log(e)
+	}
+
+})
+
+app.post('/subscribe', (req, res) => {
+
+	try {
+
+		const { subscriber } = req.body
+
+		subscribers.push(subscriber)
+
+		res.send({
+			status: 200
+		}).end()
+
+	} catch(e) {
+		console.log(e)
+	}
+
+})
+
+app.post('/broadcast', (req, res) => {
+
+	try {
+
+		const { text } = req.body
+
+		subscribers.map( element => {
+
+			pushManager.sendNotification(element, text)
+
+		})
+		
+		res.send({
+			status: 200
+		}).end()
+
+	} catch(e) {
+		console.log(e)
+	}
+
+})
+
+app.listen(port, () => console.log(`ready at http://localhost:${port}`))
